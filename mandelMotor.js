b10b875517ee1a83140ed07ec51ae35435a35784
PicturElements <namnpseudonym@gmail.com>
2016-06-05 19:57:16 +0200

    Update mandelMotor.js

--- mandelMotor.js
+++ mandelMotor.js
@@ -506,7 +506,7 @@ function pushFunction(inId){
 
 function pushFunction2(){
   $('#body').find('script').first().remove();
-  $("#body").prepend("<script id="customscript"></script>");
+  $("#body").prepend("<script id=\"customscript\"></script>");
   advancedGen=true;
   resetStuff();
   var rePart="0.125",imPart="0.8";
