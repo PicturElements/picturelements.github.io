f38f125556c403575d06714299048f3e8144ea12
PicturElements <namnpseudonym@gmail.com>
2016-06-05 11:40:56 +0200

    Update parser.js

--- formulaparser/parser.js
+++ formulaparser/parser.js
@@ -17,22 +17,22 @@ function parse(input){
             if (isNumber(extFormula.charAt(i+1))){
               tmp+=cAt;
             }else if(isOperator(extFormula.charAt(i+1))){
-              if (tmp!=""){splitFormula.push(tmp,cAt);}   //tmp=number, cAt=operator
+              splitFormula.push(tmp,cAt);   //tmp=number, cAt=operator
               tmp="";
             }
           }else if(isNumber(extFormula.charAt(i-1))){
-            if (tmp!=""){splitFormula.push(tmp,cAt);}   //tmp=number, cAt=operator
+            splitFormula.push(tmp,cAt);   //tmp=number, cAt=operator
             tmp="";
           }
         }else{
-          if (tmp!=""){splitFormula.push(tmp,cAt);}   //tmp=number, cAt=operator
+          splitFormula.push(tmp,cAt);   //tmp=number, cAt=operator
           tmp="";
         }
       }else{
         tmp+=cAt;
       }
     }
-    if (tmp!=""){splitFormula.push(tmp);}   //tmp=number, cAt=operator
+    splitFormula.push(tmp);   //tmp=number, cAt=operator
     console.log(splitFormula.length);
     for (var i=0;i<splitFormula.length;i++){
       console.log(splitFormula[i]);
