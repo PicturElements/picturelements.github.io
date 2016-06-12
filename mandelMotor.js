c1cbf01445c02b2849c3a2bbab2aa04942376d27
PicturElements <namnpseudonym@gmail.com>
2016-06-13 01:34:25 +0200

    Update mandelMotor.js

--- mandelMotor.js
+++ mandelMotor.js
@@ -19,7 +19,7 @@ var eX,eY,eOffX=1,eOffY=1;
 
 function init(){
   for (var i=0;i<100;i++){
-    $('customscripts').append("<script id="customscript"></script>");
+    $('customscripts').append("<script id=\"customscript\"></script>");
   }
   ctx2=document.getElementById("overlaycanvas").getContext("2d");
   ctx3=document.getElementById("escapecanvas").getContext("2d");
