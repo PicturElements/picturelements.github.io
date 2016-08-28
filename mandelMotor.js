2dc69f3a11ab7d911eaf3757e35471b6c5291fa1
PicturElements <namnpseudonym@gmail.com>
2016-08-28 16:55:30 +0200

    Update mandelMotor.js

--- mandelMotor.js
+++ mandelMotor.js
@@ -165,7 +165,7 @@ function confirmGradient(){
 function mainGenerate(){
   ctx=document.getElementById("mandelcanvas").getContext("2d");
   document.getElementById("upperinfo").style.display="none";
-  if (imgData!=null){ctx.putImageData(imageData,cmx,cmy);}
+  if (imgData!=null){ctx.putImageData(imgData,cmx,cmy);}
   imgData=ctx2.createImageData(width,10);
   scan=0;
   if (mode==3){mode=0;}
