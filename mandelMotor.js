4a20dfe9a9fe2e22f39fb57f1b0f9dae0979188e
PicturElements <namnpseudonym@gmail.com>
2016-08-30 13:07:04 +0200

    Update mandelMotor.js

--- mandelMotor.js
+++ mandelMotor.js
@@ -738,7 +738,7 @@ function colorMove(event){
     div.style.top=""+(yPos/vw-1.2)+"vw";
     var ctx=document.getElementById("pickercanvas").getContext("2d");
     var id=Math.floor(((yPos-6.5*vw)/23*vw)*64)*64+Math.floor(((xPos-vw)/23*vw)*64);
-    console.log(id):
+    console.log(id);
   }else{
     div.style.display="none";
   }
