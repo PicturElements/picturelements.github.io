e69f480c7979c69a780408b265ff63999391bb1c
PicturElements <namnpseudonym@gmail.com>
2016-09-02 23:34:12 +0200

    Update mandelMotor.js

--- mandelMotor.js
+++ mandelMotor.js
@@ -144,7 +144,8 @@ function parseUrl(){
 function fromHex(inStr){
   selectedCol=7;
   lengths[0]=inStr.length;
-  if (inStr.length>3){document.getElementsByClassName("coloredit")[0].style.backgroundImage="url(images/pen.png)"; element.innerHTML=""}
+  var element=document.getElementsByClassName("coloredit")[0];
+  if (inStr.length>3){element.style.backgroundImage="url(images/pen.png)"; element.innerHTML=""}
   for (var i=0;i<inStr.length;i++){
     var parsed=inStr.charCodeAt(i);
     var tmp=(parsed>=48&&parsed<58)?(parsed-48)*17:(parsed-55)*17;
